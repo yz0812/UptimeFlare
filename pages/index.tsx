51702c64a85eaab2950da63fc614b4453952666d
Mr yuan <31950358+yz0812@users.noreply.github.com>
2024-06-27 02:12:53 +0800

    修改下面的提示

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,23 +68,6 @@ export default function Home({
         )}
 
         <Divider mt="lg" />
-        <Text size="xs" mt="xs" mb="xs" style={{
-          textAlign: 'center'
-        }}>
-          Open-source monitoring and status page powered by{' '}
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            Uptimeflare
-          </a>{' '}
-          and{' '}
-          <a href="https://www.cloudflare.com/" target="_blank">
-            Cloudflare
-          </a>
-          , made with ❤ by{' '}
-          <a href="https://github.com/lyc8503" target="_blank">
-            lyc8503
-          </a>
-          .
-        </Text>
       </main>
     </>
   )
