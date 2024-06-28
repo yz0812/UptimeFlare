04d0952e6a0a51efc9f9cf1ba6dc6a2136fc3f96
Mr yuan <31950358+yz0812@users.noreply.github.com>
2024-06-28 11:57:08 +0800

    修改bug

--- uptime.config.ts
+++ uptime.config.ts
@@ -102,10 +102,10 @@ const workerConfig = {
       // `method` should be `TCP_PING` for tcp monitors
       method: 'GET',
       // `target` should be `host:port` for tcp monitors
-      target: 'http://www.aicnn.cn/',
+      target: 'https://www.aicnn.cn/',
       expectedCodes: [200,403],
       tooltip: 'GPT-4 L站[aicnn]',
-      statusPageLink: 'http://www.aicnn.cn/',
+      statusPageLink: 'https://www.aicnn.cn/',
       timeout: 10000,
     },
     {
